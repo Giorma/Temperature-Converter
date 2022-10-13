aa0911d3072ab9c066f186e2442c9cc87466230b
Giorma <giomaisuradze30@gmail.com>
2022-10-13 22:02:20 +0400

    Second-Commit

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 TemperatureType = str(input("Enter Temperature Type: C or F: "))
 
-if TemperatureType.upper() == 'C':
+if  TemperatureType.upper() == 'F':
+    F = float(input("Enter Temperature in Fahrenheit: "))
+    Celsius = (F - 32) * 5 / 9
+    print(Celsius)
+
+elif TemperatureType.upper() == 'C':
     C = float(input("Enter Temperature in Celsius: "))
     Fahrenheit = (C * 9 / 5) + 32
     print(Fahrenheit)
 
-elif TemperatureType.upper() == 'F':
-    F = float(input("Enter Temperature in Fahrenheit: "))
-    Celsius = (F - 32) * 5 / 9
-    print(Celsius)
 
 else:
     print("Try Again, Something is Wrong!")
